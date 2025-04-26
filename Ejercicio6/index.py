e36264998974ccb75a2bb02f165eafa256f29ba0
FredBrave <freddy.bravo.robalino@gmail.com>
2025-04-26 13:12:43 -0500

    Arreglando Ejercicio 6

--- Ejercicio6/index.py
+++ Ejercicio6/index.py
@@ -1,9 +1,42 @@
-from src.Metodos import Votante, LugarVotacion, PadronElectoral, Provincia
-
-
+from src.Metodos import Votante, Lugar, PadronElectoral, Provincia, Fecha
+import random
 
 def prueba():
+    fecha = Fecha(2025,4,26)
+    Eleccion = PadronElectoral(fecha=fecha)
+    for i in range(1, 11):
+        provincia = Provincia()
+        Eleccion.agregar_provincias(provincia)
+        for i in range(1,5):
+            lugar = Lugar()
+            Eleccion.agregar_lugar_votacion(lugar)
+    lugares = Eleccion.lista_lugares
+    provincias = Eleccion.lista_provincias
+    codigos_lugares =  [lugar.codigo_lugar_votacion for lugar in lugares]
+    for i in range(1,26):
+        provincia_random = random.choice(provincias)
+        provincia_random.agregar_votante(Votante(codigos_lugares))
     
+    dnis = []
+    
+    for provincia in Eleccion.lista_provincias:
+        print(f"Provincia: {provincia.nombre}")
+        if hasattr(provincia, 'votantes'):
+            for votante in provincia.votantes:
+                print(f" - {votante.nombre} ({votante.dni})")
+                dnis.append(votante.dni)
+    else:
+        print(" - No tiene votantes asignados aún.")
+
+    provincia_random = random.choice(provincias)
+    dni_random = random.choice(dnis)
+    print(f'Se buscara al votante con el dni {dni_random} en la provincia {provincia_random.nombre}')
+    VoF, registro = Eleccion.Consultar_Lugar_Votacion(provincia_random, dni_random, lugares)
+
+    if VoF:
+        print(f"Votante encontrado: {registro}")
+    else:
+        print("Votante no encontrado en esta provincia.")
 
 if __name__ == "__main__":
     prueba()
--- Ejercicio6/src/Metodos.py
+++ Ejercicio6/src/Metodos.py
@@ -1,22 +1,29 @@
+from faker import Faker
+import random
 
 class Lugar:
-    def __init__(self, codigo_lugar_votacion, direccion, localidad):
-        self.codigo_lugar_votacion = codigo_lugar_votacion
-        self.direccion = direccion
-        self.localidad = localidad
+    fake = Faker('es_ES')
+    def __init__(self):
+        self.codigo_lugar_votacion = self.fake.random_int(min=1000, max=9999) 
+        self.direccion = self.fake.street_address() 
+        self.localidad = self.fake.city()  
 
 
 class Votante:
-    def __init__(self, dni, nombre, codigo_lugar, mesa):
-        self.dni = dni
-        self.nombre = nombre
-        self.codigo_lugar_votacion = codigo_lugar
-        self.mesa = mesa
+    fake = Faker('es_ES')
+    def __init__(self, codigos_lugares):
+        self.dni = self.fake.random_int(min=10000000, max=49999999)
+        self.nombre = self.fake.name()
+        self.codigo_lugar_votacion = random.choice(codigos_lugares)  # elegimos uno de los códigos
+        self.mesa = self.fake.random_int(min=1, max=500)
+
 
 
 class Provincia:
-    def __init__(self, nombre):
-        self.nombre = nombre
+    fake = Faker('es_ES')
+
+    def __init__(self):
+        self.nombre = self.fake.state()
         self.votantes = []
 
     def agregar_votante(self, votante):
@@ -32,8 +39,8 @@ class Fecha:
         
 
 class PadronElectoral:
-    def __init__(self, fecha_actualizacion):
-        self.fecha_actualizacion = fecha_actualizacion
+    def __init__(self, fecha):
+        self.fecha = fecha
         self.lista_provincias = []  
         self.lista_lugares = []    
 
@@ -42,3 +49,26 @@ class PadronElectoral:
 
     def agregar_lugar_votacion(self, lugar):
         self.lista_lugares.append(lugar)
+    
+    def Consultar_Lugar_Votacion(self, provincia, dni_votante, lista_lugares):
+        if hasattr(provincia, 'votantes'):
+            for votante in provincia.votantes:
+                if votante.dni == dni_votante:
+                    direccion = ''
+                    localidad = ''
+                    mesa = votante.mesa
+
+                    for lugar in lista_lugares:
+                        if lugar.codigo_lugar_votacion == votante.codigo_lugar_votacion:
+                            direccion = lugar.direccion
+                            localidad = lugar.localidad
+                            break
+
+                    registro = {
+                        'direccion': direccion,
+                        'localidad': localidad,
+                        'mesa': mesa
+                    }
+                    return True, registro
+
+        return False, None
